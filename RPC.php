37e46102a3f796ddf14e63ba22937b1bd51f84d5
Daniel Convissor <danielc@php.net>
2006-04-16 15:04:53 +0000

    * Coding standards.
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_RPC/trunk@211479 c90b9560-bf6c-de11-be94-00142212c4b1

--- RPC.php
+++ RPC.php
@@ -1944,7 +1944,7 @@ function XML_RPC_encode($php_val)
 
     case 'string':
     case 'NULL':
-        if(ereg('^[0-9]{8}\T{1}[0-9]{2}\:[0-9]{2}\:[0-9]{2}$', $php_val)) {
+        if (ereg('^[0-9]{8}\T{1}[0-9]{2}\:[0-9]{2}\:[0-9]{2}$', $php_val)) {
             $XML_RPC_val->addScalar($php_val, $GLOBALS['XML_RPC_DateTime']);
         } elseif (preg_match('/[^\x20-\x7E\x09\x0A\x0D]/', $php_val)) {
             // Characters other than alpha-numeric, punctuation, SP, TAB,
