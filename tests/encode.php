9935f7619ec11d8d34377c59bc751e76f05a8bf0
Daniel Convissor <danielc@php.net>
2010-01-07 15:12:07 +0000

    add an encode test.
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_RPC/trunk@293222 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/encode.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Tests encoding values.
+ *
+ * If you are running this test from a SVN checkout, you must rename the working
+ * directory from "XML_RPC" to "XML".
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Web Services
+ * @package    XML_RPC
+ * @author     Daniel Convissor <danielc@php.net>
+ * @copyright  2005-2010 The PHP Group
+ * @license    http://www.php.net/license/3_01.txt  PHP License
+ * @version    SVN: $Id: extra-lines.php 293218 2010-01-07 14:20:08Z danielc $
+ * @link       http://pear.php.net/package/XML_RPC
+ * @since      File available since Release 1.4.4
+ */
+
+/*
+ * If the package version number is found in the left hand
+ * portion of the if() expression below, that means this file has
+ * come from the PEAR installer.  Therefore, let's test the
+ * installed version of XML_RPC which should be in the include path.
+ *
+ * If the version has not been substituted in the if() expression,
+ * this file has likely come from a SVN checkout or a .tar file.
+ * Therefore, we'll assume the tests should use the version of
+ * XML_RPC that has come from there as well.
+ */
+if ('@package_version@' != '@'.'package_version'.'@') {
+    /**
+     * Get the needed class from the PEAR installation
+     */
+    require_once 'XML/RPC.php';
+} else {
+    if (substr(dirname(__FILE__), -9, -6) != 'XML') {
+        echo "The parent directory must be named 'XML'.\n";
+        exit;
+    }
+
+    ini_set('include_path', '../../'
+            . PATH_SEPARATOR . '.' . PATH_SEPARATOR
+            . ini_get('include_path')
+    );
+
+    /**
+     * Get the needed class from the parent directory
+     */
+    require_once '../RPC.php';
+}
+
+$input = array(10, 11, 12);
+
+$expect = <<<EOT
+<?xml version="1.0" encoding="UTF-8"?>
+<methodCall>
+<methodName>nada</methodName>
+<params>
+<param>
+<value><array>
+<data>
+<value><int>10</int></value>
+<value><int>11</int></value>
+<value><int>12</int></value>
+</data>
+</array></value>
+</param>
+</params>
+</methodCall>
+EOT;
+
+$expect = trim(preg_replace("/\r\n/", "\n", $expect));
+
+$msg = new XML_RPC_Message('nada', array(XML_RPC_encode($input)));
+$msg->createPayload();
+$actual = trim(preg_replace("/\r\n/", "\n", $msg->payload));
+if ($actual == $expect) {
+    echo "passed\n";
+} else {
+    echo "PROBLEM\n";
+    echo $actual;
+}
+
+$msg = new XML_RPC_Message('nada',
+    array(
+        new XML_RPC_Value(
+            array(
+                new XML_RPC_Value(10, 'int'),
+                new XML_RPC_Value(11, 'int'),
+                new XML_RPC_Value(12, 'int'),
+            ),
+            'array'
+        )
+    )
+);
+$msg->createPayload();
+$actual = trim(preg_replace("/\r\n/", "\n", $msg->payload));
+if ($actual == $expect) {
+    echo "passed\n";
+} else {
+    echo "PROBLEM\n";
+    echo $actual;
+}
